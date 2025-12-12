46fa85f6df83de72234899f0b254eb816eb13052
Sanmathi87 <sanmathisanjay30@gmail.com>
2025-12-12 13:28:45 +0530

    Add files via upload

---
+++ SAMPLE 5.py
@@ -0,0 +1,11 @@
+r=int(input("Enter the radius:"))
+h=int(input("Enter the height:"))
+vol=3.14*r*r*h
+print("Volume of Cylinder id:",vol)
+
+
+Output:
+
+Enter the radius:45
+Enter the height:60
+Volume of Cylinder id: 381510.00000000006
