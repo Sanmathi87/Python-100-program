b262655694f8cf2f235f08595b9b306da14c56b5
Sanmathi87 <sanmathisanjay30@gmail.com>
2025-12-12 13:58:35 +0530

    Add files via upload

---
+++ SAMPLE 8.py
@@ -0,0 +1,20 @@
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+c=input("Enter the operation +,-,*,/,*:")
+print("The result is:",end='')
+if c=='+':
+    print(a+b)
+elif c=='-':
+    print(a-b)
+elif c=='/':
+    print(a/b)
+elif c=='*':
+    print(a*b)
+else:
+    print("Error:Wrong operator entered")
+
+Output:
+Enter 1st number:45
+Enter 2nd number:300
+Enter the operation +,-,*,/,*:*
+The result is:13500    
