dadfb74c9b36715afcb571692f367eba883fe088
Sanmathi87 <sanmathisanjay30@gmail.com>
2025-12-12 13:41:33 +0530

    Add files via upload

---
+++ SAMPLE 6.py
@@ -0,0 +1,11 @@
+p=int(input("Enter Principal:"))
+r=int(input("Enter Rates:"))
+t=int(input("Enter Time:"))
+si=(p*r*t)/100
+print("Simple Interest is:",si)
+
+Output:
+Enter Principal:50000
+Enter Rates:6
+Enter Time:2
+Simple Interest is: 6000.0    
