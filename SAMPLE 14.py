1bdca48ad3633f25531948ad81cd7c8af30a9510
Sanmathi87 <sanmathisanjay30@gmail.com>
2025-12-26 13:56:56 +0530

    Add files via upload

---
+++ SAMPLE 14.py
@@ -0,0 +1,15 @@
+a=[]
+n=int(input("Enter number of elements:"))
+for i in range(1,n+1):
+    b=int(input("Enter element:"))
+    a.append(b)
+a.sort()
+print("Second largest element is:",a[n-2])
+
+Output:
+Enter number of elements:4
+Enter element:3
+Enter element:5
+Enter element:9
+Enter element:8
+Second largest element is: 8    
