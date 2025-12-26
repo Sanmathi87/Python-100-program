d0a8d27c75563cbae33cf479f3d02393b9f52b2c
Sanmathi87 <sanmathisanjay30@gmail.com>
2025-12-26 14:14:49 +0530

    Add files via upload

---
+++ SAMPLE 20.py
@@ -0,0 +1,9 @@
+L=[2,58,95,999,65,32,15,1,7,45]
+n=int(input("Enter the number:"))
+print("The frequency of number",n," is ",L.count(n))
+
+Output:
+Enter the number:65
+The frequency of number 65  is  1
+
+    
