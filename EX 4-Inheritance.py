a63092329db1d837da2a640df3baadcdb80cd825
Sanmathi87 <sanmathisanjay30@gmail.com>
2025-12-30 16:07:55 +0530

    Add files via upload

--- EX 4-Inheritance.py
+++ EX 4-Inheritance.py
@@ -1,22 +1,41 @@
+
 class Student:
     def getStudentDetails(self):
-        self.rollno = input("Enter Roll Number :")
-        self.name = input("Enter Name :")
-        self.physics = int(input("Enter Physics Marks :"))
-        self.chemistry = int(input("Enter Chemistry Marks :"))
-        self.maths = int(input("Enter Math Marks :"))
+        self.rollno = input("Enter Roll Number : ")
+        self.name = input("Enter Name : ")
 
-class SDetails(Student):
+
+
+class Marks:
+    def getMarks(self):
+        self.python = int(input("Enter Python Marks : "))
+        self.java = int(input("Enter java Marks : "))
+        self.maths = int(input("Enter Maths Marks : "))
+
+
+class Result:
+    def calculatePercentage(self):
+        self.percentage = int((self.python + self.java + self.maths) / 300 * 100)
+
+
+
+class SDetails(Student, Marks, Result):
     def printResult(self):
-        self.percentage = (int)((self.physics + self.chemistry + self.maths) / 300 * 100)
-        print("Roll no:",self.rollno,"\nName:",self.name,"\nPercentage:",self.percentage)
+        output = (self.rollno, self.name, self.percentage)
+        print(output)
+
+
 
 S1 = SDetails()
 S1.getStudentDetails()
-print("Result:")
+S1.getMarks()
+S1.calculatePercentage()
+
+print("Result :")
 S1.printResult()
 
-if (S1.percentage > 70):
-    S1.physics += 9
-    print("result after adding grace mark....")
+if S1.percentage > 70:
+    S1.python += 5
+    S1.calculatePercentage()
+    print("Result after adding grace marks...")
     S1.printResult()
